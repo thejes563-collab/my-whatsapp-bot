b029468caa76109795c84c8c2049cba92d9451c8
thejes563-collab <thejes563@gmail.com>
2026-09-17 16:50:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,26 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/webhook", methods=["GET", "POST"])
-def webhook():
-    if request.method == "GET":
-        mode = request.args.get("hub.mode")
-        token = request.args.get("hub.verify_token")
-        challenge = request.args.get("hub.challenge")
-        
-        if mode and token:
-            if mode == "subscribe" and token == "mywhatsappbot123":
-                return challenge, 200
-            else:
-                return "Forbidden", 403
-        return "Hello World", 200
 
-    elif request.method == "POST":
-        data = request.json
-        print(data)
-        return "EVENT_RECEIVED", 200
+@app.route("/", methods=["GET", "POST"])
+def webhook():
+  if request.method == "GET":
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode and token:
+      if mode == "subscribe" and token == "whatsapp076":
+        return challenge, 200
+      else:
+        return "Forbidden", 403
+    return "Hello World", 200
+
+  elif request.method == "POST":
+    data = request.json
+    print(data)
+    return "EVENT_RECEIVED", 200
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=10000)
